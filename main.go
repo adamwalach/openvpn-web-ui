5adb3b8b759802c77bd5ea44495f139c956eb638
Boris Rybalkin <ribalkin@gmail.com>
2019-12-15 12:32:11 +0000

    config file path

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"os"
+	"flag"
+	"fmt"
 	"github.com/adamwalach/openvpn-web-ui/lib"
 	_ "github.com/adamwalach/openvpn-web-ui/routers"
 	"github.com/astaxie/beego"
 )
 
+func init() {
+	fmt.Println("init")
+	configFile := flag.String("config", "conf/app.conf", "Path to config file")
+	flag.Parse()
+	fmt.Println("Config file", *configFile)
+	err := beego.LoadAppConfig("ini", *configFile)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	lib.AddFuncMaps()
-	beego.AppConfigPath = os.Getenv("CONFIG_FILE")
 	beego.Run()
 }
